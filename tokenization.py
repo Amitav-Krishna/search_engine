d2ff0dd95286cf6159b0303ea37c5712a3946546
Amitav Krishna <akrishna0511@bhncdsb.ca>
2025-05-27 09:15:42 -0400

    Fix training data function

--- tokenization.py
+++ tokenization.py
@@ -1,5 +1,18 @@
 import re
 
-def tokenization(string):
-    tokens = re.findall(r"\w+|'\w+|[^\w\s]", string)
+def tokenization(string, lowercase=True, keep_apostrophes=True, split_hyphens=False):
+    if lowercase:
+            string = string.lower()
+    
+    if keep_apostrophes:
+        string = re.sub(r"'\w\b", "", string)  
+    
+    tokens = []
+    if split_hyphens:
+        tokens = re.findall(r"\w+(?:'\w+)?|[^\w\s]", string)
+    else:
+        tokens = re.findall(r"\w+(?:[-']\w+)*|[^\w\s]", string)
+    
+    tokens = [t for t in tokens if t and t != "'"]
+    
     return tokens
